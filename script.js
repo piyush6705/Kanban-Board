2d5f53a044b710a438fabf64f5da33cbde8843be
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-30 03:52:11 +0530

    im sleepu

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 
+const todo = document.querySelector('#todo');
+const progress = document.querySelector('#progress');
+const done = document.querySelector('#done');
 
+console.log(todo,progress,done);
+
+const tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task =>{
+  task.addEventListener("drag", (e) => {
+   // console.log("dragging",e);
+  })
+})
+
+progress.addEventListener("dragenter", (e) =>{
+  progress.classList.add("hover-over");
+})
+
+
+progress.addEventListener("dragleave", (e) =>{
+  progress.classList.remove("hover-over");
+})
