a3a50a1a3ca00ae33519ec67f2eaf7dde9029020
piyush6705 <sharmapiyush6705@gmail.com>
2026-06-30 16:50:35 +0530

    added new feature in when on click on button we can add new tasks but its incomplete

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const todo = document.querySelector('#todo');
 const progress = document.querySelector('#progress');
 const done = document.querySelector('#done');
+let dragElement = null;
 
 console.log(todo,progress,done);
 
@@ -9,15 +10,48 @@ const tasks = document.querySelectorAll('.task');
 
 tasks.forEach(task =>{
   task.addEventListener("drag", (e) => {
-   // console.log("dragging",e);
+   //console.log("dragging",e);
+   dragElement =task;
   })
 })
 
-progress.addEventListener("dragenter", (e) =>{
-  progress.classList.add("hover-over");
+
+function addDragEventsOnColumn(column){
+  column.addEventListener("dragenter",(e) => {
+    e.preventDefault();
+    column.classList.add("hover-over");
+  })
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("hover-over");
+  })
+
+  column.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  })
+
+  column.addEventListener("drop", (e) => {
+    e.preventDefault();
+    
+    console.log("Dropped", dragElement, column);
+
+    column.appendChild(dragElement);
+  })
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
+
+
+const toggleModalButton = document.querySelector('#toggle-modal');
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
+
+toggleModalButton.addEventListener("click", () => {
+  modal.classList.toggle("active")
 })
 
-
-progress.addEventListener("dragleave", (e) =>{
-  progress.classList.remove("hover-over");
+modalBg.addEventListener("click", () => {
+  modal.classList.remove("active")
 })
